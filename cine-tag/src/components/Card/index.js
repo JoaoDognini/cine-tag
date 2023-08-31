25a197e9a2605fdd2896460446ab50c8ddf381b7
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2023-08-31 10:55:10 -0300

    Aula 4
    Criando página de erro para rota inexistente, criando página de Player para reproduzir o vídeo selecionado e refatorado código de routes.js, utilizando Outlet.

--- cine-tag/src/components/Card/index.js
+++ cine-tag/src/components/Card/index.js
@@ -2,6 +2,7 @@ import { useFavoritoContext } from 'contextos/Favoritos';
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
 import iconeFavoritado from './favoritado.png';
+import { Link } from 'react-router-dom';
 
 export default function Card({ id, titulo, capa }) {
   const { favorito, adicionarFavorito } = useFavoritoContext();
@@ -9,11 +10,13 @@ export default function Card({ id, titulo, capa }) {
   const icone = !ehFavorito ? iconeFavoritar : iconeFavoritado;
   return (
     <div className={styles.container}>
-      <img
-        className={styles.capa}
-        src={capa}
-        alt={titulo} />
-      <h2>{titulo}</h2>
+      <Link className={styles.link} to={`/${id}`}>
+        <img
+          className={styles.capa}
+          src={capa}
+          alt={titulo} />
+        <h2>{titulo}</h2>
+      </Link>
       <img
         className={styles.favoritar}
         src={icone}
