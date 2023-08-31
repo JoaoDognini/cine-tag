d48e96bdaec04bf3b88ed03c569f0fac56a0c267
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2023-08-31 11:46:37 -0300

    Aula 5
    Transformando arquivo json do projeto em um protótipo de API e consumindo os dados recebidos.

--- cine-tag/src/components/Card/index.js
+++ cine-tag/src/components/Card/index.js
@@ -6,22 +6,28 @@ import { Link } from 'react-router-dom';
 
 export default function Card({ id, titulo, capa }) {
   const { favorito, adicionarFavorito } = useFavoritoContext();
+  
   const ehFavorito = favorito.some(fav => fav.id === id);
   const icone = !ehFavorito ? iconeFavoritar : iconeFavoritado;
+  
   return (
     <div className={styles.container}>
-      <Link className={styles.link} to={`/${id}`}>
+      <div>
+        <Link className={styles.link} to={`/${id}`}>
+          <img
+            className={styles.capa}
+            src={capa}
+            alt={titulo}
+          />
+          <h2>{titulo}</h2>
+        </Link>
         <img
-          className={styles.capa}
-          src={capa}
-          alt={titulo} />
-        <h2>{titulo}</h2>
-      </Link>
-      <img
-        className={styles.favoritar}
-        src={icone}
-        alt='Favoritar filme'
-        onClick={() => adicionarFavorito({ id, titulo, capa })} />
+          className={styles.favoritar}
+          src={icone}
+          alt='Favoritar filme'
+          onClick={() => adicionarFavorito({ id, titulo, capa })}
+        />
+      </div>
     </div>
   )
 }
--- cine-tag/src/pages/Inicio/index.js
+++ cine-tag/src/pages/Inicio/index.js
@@ -1,10 +1,18 @@
 import Titulo from 'components/Titulo';
 import Card from 'components/Card';
-import videos from 'json/db.json';
 import styles from './Inicio.module.css';
 import Banner from 'components/Banner';
+import { useEffect, useState } from 'react';
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/JoaoDognini/cine-tag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => setVideos(dados))
+  }, [])
+
   return (
     <>
       <Banner imagem="home" />
